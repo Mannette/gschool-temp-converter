b6d22e49a8da6f7eb2c3fec3bb7511fca4d9caba
ethan <ethanmannette@gmail.com>
2015-07-16 20:35:12 -0600

    Updated html and css files. Added additional functionality to javascript file. Conversion results undefined. Needs fixing.

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var temperatures = function(t) {
-  var input, output, result;
+  var input, output, fah, cel;
 
   input = document.getElementById('temp').valueOf();
 
-  if () {
+  if (t === "F") {
+    fah = input * 1.8 + 32;
+    fah = output;
 
   }
+  if (t === "C") {
+    cel = (input - 32) / 1.8;
+    cel = output;
+  }
+  document.getElementById('result').innerHTML = output;
 
 }
+
+document.getElementById('fahrenheit').addEventListener('click', function(){temperatures('F')})
+
+document.getElementById('celsius').addEventListener('click', function(){temperatures('C')})
